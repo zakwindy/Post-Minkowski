b4d14cff45aa20811800bc1996a670754c23a576
Zackary Windham <zwindham@byu.edu>
2020-05-18 12:03:14 -0400

    Add new build files and an unsuccessful attempt to write a CMakeLists.txt file

---
+++ build/CMakeFiles/binary.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zackarywindham/Research/grav_waves/src/body.cpp" "/Users/zackarywindham/Research/grav_waves/build/CMakeFiles/binary.dir/src/body.cpp.o"
+  "/Users/zackarywindham/Research/grav_waves/src/main.cpp" "/Users/zackarywindham/Research/grav_waves/build/CMakeFiles/binary.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/zackarywindham/Research/grav_waves/build/CMakeFiles/body.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/binary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary.dir/src/body.cpp.o"
+  "CMakeFiles/binary.dir/src/main.cpp.o"
+  "binary"
+  "binary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
