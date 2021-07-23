8661fa190a9d65601180e590b13a08a4a8d35a40
Zackary Windham <zwindham@byu.edu>
2021-07-23 15:34:46 -0400

    Got the script to format equations to run correctly

--- format_eqautions.jl
+++ format_eqautions.jl
@@ -15,6 +15,8 @@ function write_equations()::Cint
     output = open("formatted_equations.jl", "w+")
 
     lastline = ""
+    ibody = 0.0
+    idim = 1
     while ! eof(equations)
         line = readline(equations)
 
@@ -23,9 +25,28 @@ function write_equations()::Cint
             lastline = string(lastline, line)
         elseif lastline != ""
             lastline = string(lastline, line)
+            lastline = fix_dot(lastline)
+
+            index = findfirst('=', lastline)
+            lastline = chop(lastline, head = index - 1)
+            if ibody < nbody
+                lastline = string("du[", idim, ']', lastline)
+                idim += 1
+                if mod(idim - 1, ndim) == 0
+                    idim += ndim
+                    ibody += 0.5
+                    if ibody == (nbody / 2.0)
+                        idim = ndim + 1
+                    end
+                end
+            else
+                lastline = string("u[", (2 * nbody * ndim) + 1, ']', lastline)
+            end
+
             write(output, lastline, "\n")
             lastline = ""
         elseif line[1] == 'o'
+            line = fix_dot(line)
             write(output, line, "\n")
         end
 
@@ -35,6 +56,17 @@ function write_equations()::Cint
     close(output)
 
     return 0
+end
+
+function fix_dot(str)::String
+    str = replace(str, "./" => ".0/")
+    str = replace(str, ".)" => ".0)")
+    str = replace(str, ".*" => ".0*")
+    str = replace(str, ".+" => ".0+")
+    str = replace(str, ".-" => ".0-")
+    str = replace(str, ',' => '^')
+    str = replace(str, "pow" => "")
+    return str
 end
 
 write_equations()
