45d7c23abc3881798164a879b99b215fc5c7e08c
Zackary Windham <zwindham@byu.edu>
2021-12-28 15:02:18 -0500

    Tried to find a way to remove outliers for analyzing data

--- julia_code/solver/src/plots.m
+++ julia_code/solver/src/plots.m
@@ -1,6 +1,19 @@
 clear;
 close all;
 
+C = 1.0;		% set relativistic C
+mSun = 1.0; 	% set the mass to be in solar mass units
+
+C_CGS = 2.998e10;
+G_CGS = 6.674e-8;
+mSun_CGS = 1.989e33;
+AU = 1.496e14; % 1 AU in cm
+
+G = 1.0;
+M = mSun_CGS;		%units of mass
+L = M * (G_CGS / G) * ((C / C_CGS)^2);		%units of length
+T = L * C / C_CGS;		%units of time
+
 data = importdata('PMdata.csv');
 
 x1 = data(1,:);
@@ -14,6 +27,24 @@ z2 = data(9,:);
 x3 = data(13,:);
 y3 = data(14,:);
 z3 = data(15,:);
+
+a_in = data(28,:);
+e_in = data(29,:);
+i_in = data(30,:);
+w_in = data(31,:);
+Omega_in = data(32,:);
+
+a_in = a_in * L / AU;
+a_in = a_in(find(abs(a_in) < 0.05));
+
+a_out = data(33,:);
+e_out = data(34,:);
+i_out = data(35,:);
+w_out = data(36,:);
+Omega_out = data(37,:);
+
+a_out = a_out * L / AU;
+a_out = a_out(find(abs(a_out) < 1));
 
 x_mins = [min(x1),min(x2),min(x3)];
 x_maxes = [max(x1),max(x2),max(x3)];
