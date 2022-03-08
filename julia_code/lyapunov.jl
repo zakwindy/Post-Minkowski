142ebb538fcf906c632db3f264ce56978875fe28
Justin Tackett <justin.carl.tackett@gmail.com>
2022-03-08 13:20:10 -0700

    Prototype fixed; need index adjustment

--- julia_code/lyapunov.jl
+++ julia_code/lyapunov.jl
@@ -1,16 +1,22 @@
-function lyaExponent(rb, ra, pb, pa, t)
+function lyaExponent(rb, ra, pb, pa, numbodies)
     # ra and pa refer to the position and momentum of body a, similar thing for b
     n = size(ra)[1] # Number of snapshots
+    nb = numbodies # Number of bodies in simulation
+    pbd = pertBod # Body index being perturbed
 
     rsum = 0
     psum = 0
-    startTime = 10 # The time step corresponding to t = 1 N body units; chosen to be 1 to ensure virial equillibrium
+   
     endTime = 0 # If loop stops prematurely, it is recorded which step it stops on 
     phaseSpaceDistances = [] # Measured in natural log space; slope of a fitted line for these is Lyapunov exponent
 
-    for i in startTime:n
-        rsum += (rb[i] - ra[i])^2 # Might need to replace running sums with just one term, need to check
-        psum += (pb[i] - pa[i])^2 # Sum over all particles
+    for i in 1:n # Check this later
+        
+        for j in 1:nb
+            rsum += (rb[i][j] - ra[i][j])^2 # Might need to replace running sums with just one term, need to check
+            psum += (pb[i][j] - pa[i][j])^2 # Sum over all particles
+        end
+
         logdelta = .5 * log(rsum + psum)
         phaseSpace = exp(logdelta)
 
@@ -20,11 +26,9 @@ function lyaExponent(rb, ra, pb, pa, t)
             endTime = i
             break
         end
+        
     end
-    
-    
-    
-
-
+        
+    return phaseSpaceDistances
 
 end
